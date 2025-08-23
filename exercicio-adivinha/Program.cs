b064946e9d5e589a5e6e1c206ab6002729c290b1
Anna <acesbrama@gmail.com>
2025-08-23 11:57:25 -0300

    feat: atividades  23/08

---
+++ exercicio-adivinha/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("O que é, o que é?");
+Thread.Sleep(3000);
+Console.Write(" ... o que o ponteiro grande do relógio disse para o pequeno?");
+Thread.Sleep(5000);
+Console.Write(" Te vejo em um minuto. ");
---
+++ exercicio-time/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.BackgroundColor = (ConsoleColor)15;
+Console.ForegroundColor = (ConsoleColor)0;
+Console.WriteLine(" Meu time do coração é: ");
+Console.WriteLine();
+Console.ResetColor();
+
+Console.BackgroundColor = (ConsoleColor)12;
+Console.ForegroundColor = (ConsoleColor)0;
+Console.WriteLine(" Arsenal FC ");
+Console.ResetColor();

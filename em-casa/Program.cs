04efd3fff981781781591ca537a5ef31536fa724
Anna <acesbrama@gmail.com>
2025-08-31 16:43:18 -0300

    feat: aulas de concatenação e interpolação de strings

---
+++ em-casa/Program.cs
@@ -0,0 +1,18 @@
+﻿string nome = "Anna";
+string sobrenome = "Sbrama";
+
+// concatenação por operador
+string nomeCompleto = nome + " " + sobrenome;
+Console.WriteLine(nomeCompleto);
+
+//concatenação com método Concat
+string nomeConcat = String.Concat(nome, " ", sobrenome);
+Console.BackgroundColor = (ConsoleColor)4;
+Console.WriteLine(nomeConcat);
+Console.ResetColor();
+
+// interpolação
+string nomeInter = $"{nome} {sobrenome}";
+Console.BackgroundColor = (ConsoleColor)5;
+Console.WriteLine(nomeInter);
+Console.ResetColor();
